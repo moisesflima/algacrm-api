73c7639c6c8dec169a3b6642363975354db4e319
moisesflima <moisesflima@gmail.com>
2021-05-31 22:02:13 -0300

    criacao da classe cliente

---
+++ src/main/java/com/algaworks/crm/model/Cliente.java
@@ -0,0 +1,40 @@
+package com.algaworks.crm.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+}
